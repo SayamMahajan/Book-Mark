999b9923e629feb932672971800b1dd1b2189425
SayamMahajan <mahajansayam3@gmail.com>
2024-08-01 12:11:13 +0530

    update

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ const db = new pg.Client({
   user: "postgres",
   host: "localhost",
   database: "school",
-  password: "sm200488",
+  password: "#", //enter your pg admin password
   port: 5432,
 });
 
